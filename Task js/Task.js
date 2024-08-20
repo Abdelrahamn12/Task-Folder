f912f0343319787cb7ae5e37b918a534e436700c
Abdelrahamn12 <112874740+Abdelrahamn12@users.noreply.github.com>
2024-08-20 03:12:09 +0300

    Add files via upload

---
+++ Task js/Task.js
@@ -0,0 +1,39 @@
+// let x = 10; 
+
+// function boda (){
+//     let x = 20 ; 
+//     console.log(x)
+// }
+// boda() // output => 20 local Scope 
+
+ /////////////////////////////////////
+
+
+// let y = 10; 
+
+// function boda (){
+//     console.log(y)
+// }
+// boda() // 10 Global Scope
+
+// /////////////////
+
+// var z = 17; 
+
+// function boda (){
+//     var z = 50 ;
+//     console.log(z)
+// }
+// boda() // 50 Local Group
+
+/////////////////////
+
+// let a = 15; 
+
+// function boda (){
+    
+//     console.log(a)
+// }
+// boda() // 15 Global Group
+
+
